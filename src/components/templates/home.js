1cf6ab5957aa0f1f3f91c780e944d2980f76f4dd
Shubham Upreti <shubham.upreti@gmail.com>
2021-09-16 12:45:20 +0530

    app img

--- src/components/templates/home.js
+++ src/components/templates/home.js
@@ -1,31 +1,50 @@
-import { Box, Flex, Heading, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Flex, Heading, Text, Image } from '@chakra-ui/react';
+import React from 'react';
+import appImg from '../../assets/app.png';
 
 const Home = () => {
   return (
     <Flex
       h="100vh"
       justifyContent="center"
-      direction={["column", "column", "row", "row"]}
+      direction={['column', 'column', 'row', 'row']}
       wrap="wrap"
     >
-        <Box 
-          w={["auto", "auto", "50%", "55%"]}
-          h={["50%", "50%", "inherit", "inherit"]}
+      <Box
+        w={['auto', 'auto', '50%', '55%']}
+        h={['50%', '50%', 'inherit', 'inherit']}
+      ></Box>
+      <Box
+        w={['auto', 'auto', '50%', '45%']}
+        h={['50%', '50%', 'inherit', 'inherit']}
+        justifySelf="center"
+        p={3}
+      >
+        <Image src={appImg} />
+        <Heading
+          fontSize={['3em', '3em', '4em', '5em']}
+          mt={['0', '0', '6rem', '6rem']}
+          ml={['1rem', '1rem', '1rem', '3rem']}
+          color="white"
         >
-        </Box>
-        <Box 
-          w={["auto", "auto", "50%", "45%"]}
-          h={["50%", "50%", "inherit", "inherit"]}
-          justifySelf="center"
-          p={3}
+          SmartPark
+        </Heading>
+        <Text
+          fontSize="xl"
+          ml={['1rem', '1rem', '1rem', '3rem']}
+          mt={['0.3rem', '0.7rem', '1rem', '1rem']}
+          fontWeight="700"
+          color="brand.100"
         >
-          <Heading fontSize={["3em", "3em", "4em", "5em"]} mt={["0", "0", "6rem", "6rem"]} ml={["1rem", "1rem", "1rem", "3rem"]} color="white">SmartPark</Heading>
-          <Text fontSize="xl" ml={["1rem", "1rem", "1rem", "3rem"]} mt={["0.3rem", "0.7rem", "1rem", "1rem"]} fontWeight="700" color="brand.100">Smarter Contactless Parking</Text>
-          <Text fontSize="md" ml={["1rem", "1rem", "1rem", "3rem"]} color="white">Save your precious time.<br/> Book your parking slots before going out.</Text>
-        </Box>
+          Smarter Contactless Parking
+        </Text>
+        <Text fontSize="md" ml={['1rem', '1rem', '1rem', '3rem']} color="white">
+          Save your precious time.
+          <br /> Book your parking slots before going out.
+        </Text>
+      </Box>
     </Flex>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
