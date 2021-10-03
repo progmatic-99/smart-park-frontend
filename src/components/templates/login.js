71247566dab37c2410935c83c40aa62e34686a34
Shubham Upreti <shubham.upreti@gmail.com>
2021-10-03 23:59:18 +0530

    f

--- src/components/templates/login.js
+++ src/components/templates/login.js
@@ -1,60 +1,65 @@
-import { Button, Flex, Heading } from '@chakra-ui/react'
-import { Form, Formik } from 'formik'
-import { Link as ReactLink } from 'react-router-dom'
-import * as Yup from 'yup'
-import Field from '../form/formFields'
+import { Button, Flex, Heading } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
+import { Link as ReactLink } from 'react-router-dom';
+import * as Yup from 'yup';
+import Field from '../form/formFields';
 
-
-const axios = require('axios').default
+const axios = require('axios').default;
 
 const handleLogin = async (values, { resetForm, setSubmitting }) => {
   try {
-    await axios.post('/api/login', JSON.stringify(values))
-  } catch {
-    console.error(err)
+    await axios.post('/api/login', JSON.stringify(values));
+  } catch (err) {
+    console.error(err);
   }
-  setSubmitting(false)
-  resetForm()
-}
+  setSubmitting(false);
+  resetForm();
+};
 
 const loginSchema = Yup.object({
   email: Yup.string().email('Invalid email').required('Required'),
-  password: Yup.string().required('Required')
-})
+  password: Yup.string().required('Required'),
+});
 
 const Login = () => {
   return (
     <Formik
       initialValues={{
         email: '',
-        password: ''
+        password: '',
       }}
       onSubmit={handleLogin}
       validationSchema={loginSchema}
     >
-        <Form>
-          <Flex height="100vh" alignItems="flex-start" justifyContent="center">
-            <Flex direction="column" p={12} rounded={24}>
-              <Heading color="white" textAlign="center" mb={4}>SmartPark</Heading> 
-              <Field
-                label="Email"
-                placeholder="email address"
-                name="email"
-                type="email"
-              />
-              <Field
-                label="Password"
-                placeholder="password"
-                name="password"
-                type="password"
-              />
-              <Button variant="primary" type="submit" mb={6}>Log In</Button>
-              <Button variant="secondary" as={ReactLink} to="/signup" >Sign Up</Button>
-            </Flex>
+      <Form>
+        <Flex height="100vh" alignItems="flex-start" justifyContent="center">
+          <Flex direction="column" p={12} rounded={24}>
+            <Heading color="white" textAlign="center" mb={4}>
+              SmartPark
+            </Heading>
+            <Field
+              label="Email"
+              placeholder="email address"
+              name="email"
+              type="email"
+            />
+            <Field
+              label="Password"
+              placeholder="password"
+              name="password"
+              type="password"
+            />
+            <Button variant="primary" type="submit" mb={6}>
+              Log In
+            </Button>
+            <Button variant="secondary" as={ReactLink} to="/signup">
+              Sign Up
+            </Button>
           </Flex>
-        </Form>
+        </Flex>
+      </Form>
     </Formik>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
