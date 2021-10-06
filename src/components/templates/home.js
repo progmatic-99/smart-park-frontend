80eaeb88ba0a3994202e87c02a572602a909ecfa
Shubham Upreti <shubham.upreti@gmail.com>
2021-10-07 00:10:05 +0530

    home page done

--- src/components/templates/home.js
+++ src/components/templates/home.js
@@ -1,33 +1,29 @@
 import { Box, Flex, Heading, Text, Image } from '@chakra-ui/react';
 import React from 'react';
-import appImg from '../../assets/app.png';
+import appImg from '../../assets/app.webp';
 
 const Home = () => {
   return (
     <Flex
       h="100vh"
-      justifyContent="center"
-      direction={['column', 'column', 'row', 'row']}
+      w="100vw"
+      direction={['column-reverse', 'row', 'row', 'row']}
       wrap="wrap"
+      align={['center', 'flex-start', 'flex-start', 'flex-start']}
     >
-      {/* <Image
+      <Image
+        w={['100%', '50%', '55%', '55%']}
+        h={['70%', '60%', '70%', '100%']}
         src={appImg}
-        w={['auto', 'auto', '50%', '55%']}
-        h={['50%', '50%', 'inherit', 'inherit']}
-      /> */}
+      />
       <Box
-        w={['auto', 'auto', '50%', '55%']}
-        h={['50%', '50%', 'inherit', 'inherit']}
-      >
-      </Box>
-      <Box
-        w={['auto', 'auto', '50%', '45%']}
-        h={['50%', '50%', 'inherit', 'inherit']}
-        justifySelf="center"
+        w={['100%', '50%', '45%', '45%']}
+        h={['30%', '40%', '70%', '100%']}
         p={3}
+        textAlign={['center', 'left', 'left', 'left']}
       >
         <Heading
-          fontSize={['3em', '3em', '4em', '5em']}
+          fontSize={['3em', '2.5em', '3.7em', '5em']}
           mt={['0', '0', '6rem', '6rem']}
           ml={['1rem', '1rem', '1rem', '3rem']}
           color="white"
