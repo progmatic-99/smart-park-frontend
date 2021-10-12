0e02e9ce278ded0000c5b4a9db71b5e6723f96f7
Shubham Upreti <shubham.upreti@gmail.com>
2021-10-12 23:54:09 +0530

    refactor

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,3 +1,7 @@
-import { createContext } from 'react';
+import { createContext, useContext } from 'react';
 
-export const UserContext = createContext(null);
+const UserContext = createContext(null);
+
+export function useAuth() {
+  return useContext(UserContext);
+}
